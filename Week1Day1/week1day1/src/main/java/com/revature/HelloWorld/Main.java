6308b71d34473889b901ff02d0cabde105cf9252
Keven Mitchell <keven.mitchell@revature.com>
2023-06-06 12:54:59 -0400

    Week 1 Day 1 code example

---
+++ Week1Day1/week1day1/src/main/java/com/revature/HelloWorld/Main.java
@@ -0,0 +1,28 @@
+package com.revature.HelloWorld;
+/*
+ * Source code - what we write as developers
+ * Java is a compiled language
+ *  the compiler converts our source code into a lower level language
+ *  specifically bytecode for Java
+ * 
+ * Bytecode - this is fed into the JVM and interpreted into machine code
+ * 
+ * JVM - Java Virtual Machine: take bytecode as an input, and run an environment which executes that code
+ *          Write Once, Run Anywhere
+ * 
+ * JRE - Java Runtime environment: contains the JVM and the standard library, allows us to run code
+ * 
+ * JDK - Java Development Kit: Contains both of the others, as well as development tools for compiling(amongst other tasks) Java code
+ */
+import java.util.*;
+
+public class Main {
+    ArrayList<String> strings;
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World!");
+
+    }
+
+}
